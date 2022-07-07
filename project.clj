f9e03bab7ce470866f0e1556f0570740486ca2e1
Artsiom Aniskou <aani@d-velop.de>
2022-07-07 13:41:51 +0200

    - update project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject image-downloader "0.1.1-with-property"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple project for downloading images from image boards."
+  :url "https://github.com/RagnarR/image-downloader"
   :license {:name "MIT License"
             :url "https://spdx.org/licenses/MIT.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
